f71d7a60f3301355ea7a2eed790c50c0d03fc960
Juan Manuel Kobayashi <juanmanuelkobayashi@gmail.com>
2025-02-04 22:54:18 -0300

    comentado

--- src/middleware/circuitBreaker.ts
+++ src/middleware/circuitBreaker.ts
@@ -8,20 +8,25 @@ const options = {
     resetTimeout: 30000 // El circuito se cierra después de 30 segundos
 };
 
+// Creación del CircuitBreaker con una función que simplemente llama a next()
 const breaker = new CircuitBreaker(async (req: Request, res: Response, next: NextFunction) => {
     next();
 }, options);
 
+// Configuración del fallback para cuando el circuito está abierto
 breaker.fallback((req: Request, res: Response) => {
     res.status(HTTP.SERVICE_UNAVAILABLE).json({ message: ERROR_MESSAGES.SERVICE_UNAVAILABLE });
 });
 
+// Eventos del CircuitBreaker para monitorear su estado
 breaker.on('open', () => console.log('Circuito abierto'));
 breaker.on('halfOpen', () => console.log('Circuito medio abierto'));
 breaker.on('close', () => console.log('Circuito cerrado'));
 
+// Middleware que utiliza el CircuitBreaker
 export const withCircuitBreaker = (req: Request, res: Response, next: NextFunction) => {
     breaker.fire(req, res, next).catch(next);
 };
 
+// Exportación del breaker para que pueda ser utilizado en pruebas
 export { breaker };
