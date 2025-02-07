670dbd98f675f9fb3ce5914d936f619ca420b027
Juan Manuel Kobayashi <juanmanuelkobayashi@gmail.com>
2025-02-07 15:31:51 -0300

    cambie localhost

--- src/config/constants/defaults.ts
+++ src/config/constants/defaults.ts
@@ -3,7 +3,7 @@ export const DEFAULTS = {
     CACHE_EXPIRY: 3600,
     RETRY_ATTEMPTS: 3,
     RETRY_DELAY: 1000, // en milisegundos
-    REDIS_HOST: 'localhost',
+    REDIS_HOST: 'redis',
     REDIS_PORT: 6379,
     REDIS_RETRY_DELAY: 2000 // en milisegundos
 };
